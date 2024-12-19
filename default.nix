57ec85b1a619294286a5714c6bb9129ff73e07c5
Michael Smith <m@michaelsmith.be>
2024-12-19 23:27:18 +0100

    Remove default.nix superseded by flake.nix

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv, lib, qmake, qt5, wrapQtAppsHook }: 
-
-stdenv.mkDerivation rec {
-  pname = "tab-ui";
-  version = "1.0";
-  src = ./.;
-  buildInputs = [ qt5.full ];
-  nativeBuildInputs = [ qmake wrapQtAppsHook ]; 
-}
