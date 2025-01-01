b5c8a11f951e4585de9f656ac862b6648378b5f3
Michael Smith <m@michaelsmith.be>
2025-01-01 17:02:50 +0100

    Add overlay to expose tab-ui package from flake

--- flake.nix
+++ flake.nix
@@ -42,41 +42,47 @@
         function (import nixpkgs {
           inherit system;
         }));
-  in {
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
+  in
+    {
+      formatter = forAllSystems (pkgs: pkgs.alejandra);
 
-    packages = forAllSystems (pkgs: {
-      # There's only one package defined in this flake, so let's make that the default one:
-      default = pkgs.stdenv.mkDerivation rec {
-        pname = "tab-ui";
+      packages = forAllSystems (pkgs: {
+        default = self.packages.${pkgs.system}.tab-ui;
+        tab-ui = pkgs.stdenv.mkDerivation rec {
+          pname = "tab-ui";
 
-        # tab-ui does not have versioned releases. To still keep track of some sort of version (a Nix package requires
-        # it and it's also convenient for debugging) and not having to make up something arbitrary like "1.0", we'll
-        # use the Nix builtin substring function to extract the first 7 characters of the git commit hash that the
-        # build of this package is based on and use that as the version indicator.
-        # To avoid duplication or creating extra variables through let bindings, we'll make the attribute set passed to
-        # the mkDerivation function above recursive by adding the `rec` keyword. This allows us to reference the
-        # revision attribute in the fetchgit function below through `src.rev`.
-        version = builtins.substring 0 7 src.rev;
+          # tab-ui does not have versioned releases. To still keep track of some sort of version (a Nix package requires
+          # it and it's also convenient for debugging) and not having to make up something arbitrary like "1.0", we'll
+          # use the Nix builtin substring function to extract the first 7 characters of the git commit hash that the
+          # build of this package is based on and use that as the version indicator.
+          # To avoid duplication or creating extra variables through let bindings, we'll make the attribute set passed to
+          # the mkDerivation function above recursive by adding the `rec` keyword. This allows us to reference the
+          # revision attribute in the fetchgit function below through `src.rev`.
+          version = builtins.substring 0 7 src.rev;
 
-        src = pkgs.fetchgit {
-          url = "https://github.com/voidwarranties/tab-ui.git";
-          rev = "90c0d413625e53826605c5b92fcc02e5b4a8b736";
-          hash = "sha256-uQ/BIEYArVs0KIwfDNQbqxrTSbwZ6NIgW4KMwz7xSHk=";
-          fetchSubmodules = true;
+          src = pkgs.fetchgit {
+            url = "https://github.com/voidwarranties/tab-ui.git";
+            rev = "90c0d413625e53826605c5b92fcc02e5b4a8b736";
+            hash = "sha256-uQ/BIEYArVs0KIwfDNQbqxrTSbwZ6NIgW4KMwz7xSHk=";
+            fetchSubmodules = true;
+          };
+
+          nativeBuildInputs = [pkgs.qt5.qmake];
+
+          buildInputs = with pkgs.qt5; [
+            qtquickcontrols2
+            wrapQtAppsHook
+          ];
+
+          patches = [
+            ./patches/0001-main.qml-change-window-title-for-voidwarranties.patch
+          ];
         };
-
-        nativeBuildInputs = [ pkgs.qt5.qmake ];
-
-        buildInputs = with pkgs.qt5; [
-          qtquickcontrols2
-          wrapQtAppsHook
-        ];
-
-        patches = [
-          ./patches/0001-main.qml-change-window-title-for-voidwarranties.patch
-        ];
+      });
+    }
+    // {
+      overlays.default = final: prev: {
+        inherit (self.packages.${prev.system}) tab-ui;
       };
-    });
-  };
+    };
 }
