ab395ce8e1fc740a8d1f4fcc14e8c9d293671edb
snowyeline <e12l@e12l.be>
2025-03-05 23:07:23 +0100

    update flake to local reference

--- flake.nix
+++ flake.nix
@@ -3,11 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    devshell,
   } @ inputs: let
     # This list of architectures provides the supported systems to the wrapper function below.
     # It basically defines which architectures can build and run the tab-ui application.
@@ -41,30 +47,21 @@
       nixpkgs.lib.genAttrs supportedSystems (system:
         function (import nixpkgs {
           inherit system;
+          overlays = [
+            devshell.overlays.default
+          ];
         }));
   in {
     formatter = forAllSystems (pkgs: pkgs.alejandra);
 
     packages = forAllSystems (pkgs: {
       default = self.packages.${pkgs.system}.tab-ui;
-      tab-ui = pkgs.stdenv.mkDerivation rec {
+      tab-ui = pkgs.stdenv.mkDerivation {
         pname = "tab-ui";
 
-        # tab-ui does not have versioned releases. To still keep track of some sort of version (a Nix package requires
-        # it and it's also convenient for debugging) and not having to make up something arbitrary like "1.0", we'll
-        # use the Nix builtin substring function to extract the first 7 characters of the git commit hash that the
-        # build of this package is based on and use that as the version indicator.
-        # To avoid duplication or creating extra variables through let bindings, we'll make the attribute set passed to
-        # the mkDerivation function above recursive by adding the `rec` keyword. This allows us to reference the
-        # revision attribute in the fetchgit function below through `src.rev`.
-        version = builtins.substring 0 7 src.rev;
+        version = "1.0";
 
-        src = pkgs.fetchgit {
-          url = "https://github.com/voidwarranties/tab-ui.git";
-          rev = "90c0d413625e53826605c5b92fcc02e5b4a8b736";
-          hash = "sha256-uQ/BIEYArVs0KIwfDNQbqxrTSbwZ6NIgW4KMwz7xSHk=";
-          fetchSubmodules = true;
-        };
+        src = ./.;
 
         nativeBuildInputs = with pkgs; [
           qt5.qmake
@@ -77,6 +74,25 @@
 
         patches = [
           ./patches/0001-main.qml-change-window-title-for-voidwarranties.patch
+        ];
+      };
+    });
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.devshell.mkShell {
+        name = "tab-ui devshell";
+        packages = with pkgs; [
+          stdenv
+          qt5.full
+        ];
+        commands = [
+          {
+            name = "run";
+            help = "run";
+            command = ''
+              nix run '.?submodules=1#'            
+            '';
+          }
         ];
       };
     });
